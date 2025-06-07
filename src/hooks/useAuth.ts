2e557a6ea789894f86c162582b4f1bb70a833487
mangisto14 <160495661+mangisto14@users.noreply.github.com>
2025-06-07 13:39:06 +0300

    Create useAuth.ts

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../supabase/client';
+
+export function useAuth() {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const session = supabase.auth.getSession().then(({ data }) => {
+      setUser(data?.session?.user || null);
+      setLoading(false);
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user || null);
+    });
+
+    return () => {
+      listener?.subscription.unsubscribe();
+    };
+  }, []);
+
+  return { user, loading };
+}
