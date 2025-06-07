aed9615d202d703b835c3f5dcfd860af7def662d
mangisto14 <160495661+mangisto14@users.noreply.github.com>
2025-06-07 14:47:37 +0300

    Create login.js

---
+++ i18n/login.js
@@ -0,0 +1,14 @@
+export const loginLabels = {
+  he: {
+    login: "התחברות",
+    email: "דוא״ל",
+    password: "סיסמה",
+    submit: "התחבר",
+  },
+  en: {
+    login: "Login",
+    email: "Email",
+    password: "Password",
+    submit: "Log in",
+  },
+};
