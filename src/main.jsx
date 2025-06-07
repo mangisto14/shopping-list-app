4e01384f46738c89af4bf11ed73dd76d6e968366
mangisto14 <160495661+mangisto14@users.noreply.github.com>
2025-06-07 14:23:48 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { LanguageProvider } from "./LanguageContext";
 
-<LanguageProvider>
-  <App />
-</LanguageProvider>
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+  </React.StrictMode>
+);
